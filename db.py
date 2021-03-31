b5e6e164b981a52e81682868f5d0c2e54bfafb72
Leevi Kokkinen <leevi.kokkinen@gmail.com>
2021-03-31 16:08:13 +0300

    Lisätty kaikkien viestin poistamis- sekä muokkaamismahdollisuus admineille. Tämän lisäksi adminit pystyvät nyt muokata alueiden tietoja

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")#.replace("://", "ql://",1)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://",1)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
